32365de24850989ff53d26c872524521e43664ec
Myrrh Khan <myrrhkhan@gmail.com>
2023-07-15 00:43:57 -0400

    starter :)

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Header from './Header.tsx';
+
+function App() {
+  return <div><Header /></div>;
+}
+
+export default App
---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+	return <div>
+		<img 
+			src="./src/assets/headshot.png" 
+			alt="Myrrh Khan's face"
+			width="175"
+		/>
+		<h1>Myrrh Khan's Site</h1>
+	</div>
+	;
+	
+}
+
+export default Header;
