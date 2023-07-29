a72f9336cbbd52885d6220d19d63525316d5d390
Myrrh Khan <myrrhkhan@gmail.com>
2023-07-29 03:13:35 -0400

    finally got the hang of css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,31 @@
-import './Header.css'
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './Header.css';
+
+// for future reference: https://react-bootstrap.netlify.app/docs/components/navbar/
+// https://www.youtube.com/watch?v=8pKjULHzs0s
 
 function Header() {
   return (
-    <div className="container">
-      <div className="image">
-        <img
-          className="Header"
-          src="./headshot.png"
-          alt="Myrrh Khan's face"
-          width="175"
-        />
-      </div>
-      <div className="text">
-        <h1>Myrrh's Site</h1>
-      </div>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src="./headshot.png"
+            alt="Myrrh Khan's face"
+            width="100"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand" />
+            <h2 className="NavbarText">Myrrh Khan's Site</h2>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
 export default Header;
-
-/**
- *       <img
-        className="Header"
-        src="/headshot.png"
-        alt="Myrrh Khan's face"
-        width="175"
-      />
- */
