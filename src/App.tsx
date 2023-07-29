85087efb5df0143185c45a9b8d92f24860f0a3ca
Myrrh Khan <myrrhkhan@gmail.com>
2023-07-28 23:50:01 -0400

    in progress that i wanna commit lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from "./Header.tsx";
+import Header from "./components/Header.tsx";
 
 function App() {
   return (
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="container">
+      <div className="image">
+        <img
+          className="Header"
+          src="/headshot.png"
+          alt="Myrrh Khan's face"
+          width="175"
+        />
+      </div>
+      <div className="text">
+        <h1>Myrrh's Site</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
+
+/**
+ *       <img
+        className="Header"
+        src="/headshot.png"
+        alt="Myrrh Khan's face"
+        width="175"
+      />
+ */
