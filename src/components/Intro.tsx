8618f733f5fe6bcd7fa256d039d8a6aff5a36388
Myrrh Khan <myrrhkhan@gmail.com>
2023-08-06 00:34:14 -0400

    better background but height doesn't auto adjust

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,40 +1,46 @@
+import "../index.css";
+
 export default function Intro() {
   return (
-    <div style={{ height: "500px" }} className='flex flex-row'>
-      <div className='hidden sm:block' style={{ height: "600px" }}>
+    <div style={{ height: "750px" }} className='flex flex-row bg-contain'>
+      {/* <div className='hidden sm:block' style={{ height: "600px" }}>
         <img
           src='./headshot.png'
           alt="Myrrh Khan's face"
           className='object-scale-down top-left'
           style={{ height: "600px" }}
         />
-      </div>
+      </div> */}
       <div
         className='
 				flex
 				flex-col
-				sm:bg-primary-saffron 
-				bg-cover w-full'
+        overflow-hidden relative'
       >
-        <h1
-          className='
-					text-primary-persian-green
-					text-center
-					text-3xl
-					font-subheader mt-20'
-        >
-          Hi
-        </h1>
-        <p className='font-paragraph text-center text-small'>
-          I'm Myrrh (he/him/his) and I am a Computer Science student at
-          Worcester Polytechnic Institute pursuing a robotics engineering minor,
-          and I will graduate in late 2025 or spring 2026.
-        </p>
-        <p className='font-paragraph text-center text-small'>
-          I have been programming since 2019, when I joined my high school
-          robotics team. I have since programmed a variety of projects, from
-          machine learning models to systems programming to web development.
-        </p>
+        <div className=''>
+          <img className='intro-bg' src='headshot.png' alt='my face' />
+        </div>
+        <div className='relative'>
+          <h1
+            className='
+                    text-primary-persian-green
+                    text-center
+                    text-3xl
+                    font-subheader mt-20'
+          >
+            Hi
+          </h1>
+          <p className='font-paragraph text-center text-small font-bold text-primary-charcoal pl-20 pr-20'>
+            I'm Myrrh (he/him/his) and I am a Computer Science student at
+            Worcester Polytechnic Institute pursuing a robotics engineering
+            minor, and I will graduate in late 2025 or spring 2026.
+          </p>
+          <p className='font-paragraph text-center text-small font-bold text-primary-charcoal pl-20 pr-20'>
+            I have been programming since 2019, when I joined my high school
+            robotics team. I have since programmed a variety of projects, from
+            machine learning models to systems programming to web development.
+          </p>
+        </div>
       </div>
     </div>
   );
