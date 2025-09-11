a29a004fc742103731baab976cad7ea974131259
Myrrh Khan <myrrhkhan@gmail.com>
2025-09-11 03:49:35 -0400

    IQP + resume blurb

--- src/lib/components/Resume.svelte
+++ src/lib/components/Resume.svelte
@@ -10,6 +10,24 @@
 </script>
 
 <div style="background-color: var(--tf-page-bg-color)" class="bg-gradient py-3">
+	<div class="m-6">
+		<h1 class="text-2xl font-medium">Intrigued? Let's Talk!</h1>
+		<p class="text-lg sm:text-xl">
+			I'm currently looking for full-time entry-level jobs. If you like what you see, reach out to
+			me on <a
+				href="https://www.linkedin.com/in/myrrh-khan"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="text-blue-600 underline hover:text-blue-800">LinkedIn</a
+			>!
+		</p>
+		<p class="text-lg sm:text-xl">
+			For security reasons, I do not publish my personal phone number or email on this website, but
+			I'm also reachable at mskhan@wpi.edu before February 2026, or mskhan@alum.wpi.edu from
+			February 2026 onward. My Google Voice number, (339) 707-0364, also works. Rest assured, these
+			reach me just as well as my personal phone and email!
+		</p>
+	</div>
 	<div class="resume_box">
 		<button type="submit" style="width: 40%;" onclick={() => download('resumes/resume.pdf')}
 			>Download!</button
--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -17,6 +17,86 @@ export interface ProjectImage {
 
 export const projects: ProjectData[] = [
 	{
+		title:
+			'From Local Flavors to Corporate Tables: Empowering Caño Martín Peña Through Economic Growth for its Restaurants',
+		date: 'October 2024-July 2025',
+		blurb:
+			"Four-person project in the process of being published, where we spent 8 weeks in Puerto Rico connecting restaurants in the lower-income CMP community with the nearby financial district, increasing CMP's economic resilience. Completed as part of WPI's Interactive Qualifying Project (IQP) requirements.",
+		summary: '',
+		images: [
+			{
+				src: '/images/iqp/IMG_4670.jpeg',
+				alt: 'Meeting community members at an artisinal market'
+			},
+
+			{
+				src: '/images/iqp/20250114_160941833_iOS.jpg',
+				alt: 'Milla de Oro'
+			},
+			{
+				src: '/images/iqp/20250122_225751000_iOS.jpg',
+				alt: 'Meeting with Doña Ana and her home limber business'
+			},
+			{
+				src: '/images/iqp/20250210_150019110_iOS.jpg',
+				alt: 'Flyers'
+			},
+			{
+				src: '/images/iqp/20250210_154703877_iOS.jpg',
+				alt: 'Talking to corporations'
+			},
+			{
+				src: '/images/iqp/20250210_173259632_iOS.jpg',
+				alt: 'Talking to a business'
+			},
+			{
+				src: '/images/iqp/20250214_192558151_iOS.jpg',
+				alt: 'Pitching to our middleman'
+			},
+			{
+				src: '/images/iqp/20250227_200739046_iOS.jpg',
+				alt: 'Bike tour'
+			},
+			{
+				src: '/images/iqp/20250304_201444000_iOS.jpg',
+				alt: 'Final presentation at ENLACE'
+			},
+			{
+				src: '/images/iqp/20250304_202013000_iOS.jpg',
+				alt: 'Final presentation 2'
+			},
+			{
+				src: '/images/iqp/20250304_220938296_iOS.jpg',
+				alt: 'Final presentations with the rest of our WPI IQP cohort at the La Goyco community center'
+			}
+		],
+		labels: [
+			'Figma',
+			'Market Gaps',
+			'Business Creation',
+			'Academic Research',
+			'Interviewing',
+			'Stakeholder Interaction'
+		],
+		projectUrl: 'https://digital.wpi.edu/concern/student_works/47429f41s?locale=en',
+		longerNarrative: `For eight weeks, from January to March 2025, we were in Puerto Rico working with the Corporación del Proyecto ENLACE del Caño Martín Peña to connect Caño Martín Peña restaurants to the nearby financial district (Milla de Oro, or MdO). We previously spent 8 weeks doing research and devising of methods for our project, and a few months after we completed our project, we worked on a paper to be published. Whilst in Puerto Rico, we interviewed restaurants and corporations (with ENLACE employees translating) and created an outline of a delivery system, complete with pricing and a Figma mockup. During the following summer, we were granted the opportunity to publish our IQP in a journal, something not all groups were given. We decided to write about our project as a novel example of university-community partnerships, where community organizations pitched their ideas to universities rather than vice-versa.
+
+Thanks to this project, I am better able to conduct academic research and apply its findings in the real-world. I am now better able to interface with multiple stakeholders, recognize their problems, and create optimal solutions. I experienced firsthand the importance of understanding cultural context. I became significantly more able to market to complete strangers and get them on board. I finally increased my presentation skills, being able to present my project much more confidently by the end of the project compared to the beginning.
+
+The Caño Martín Peña (CMP) community is a community consisting mainly of migrant workers and their descendants. The area was informally settled along the Martín Peña canal, with self-built homes that are not up to code. These residents are susceptible to displacement, especially after the planned dredging of the canal. As such, the group Caño3.7 was established, with 3 divisions:
+• The G-8, a governing body for the 8 communities within CMP
+• The land trust, which prevents developers from buying land in the area
+• ENLACE, who we worked with, who are tasked with community outreach and increasing economic development and resilience.
+
+ENLACE noticed how restaurants in the area did not get much clientele from the nearby financial district, despite offering cheaper options.
+
+Our first 8 weeks consisted of academic research into CMP, similar community development initiatives, and marketing. Our next eight weeks had us interviewing restaurants, where we learned that these family-owned restaurants operated via cash and ATH Móvil, Puerto Rico's own version of Venmo, as using card was too expensive. They operated on a smaller scale and could not afford to use UberEats or DoorDash. We then interviewed corporations, who expressed interest in having cheaper catering options and cheaper options for employees. Using this info, we devised a business where companies could place their orders to a middleman, who could place those orders to the restaurants, removing the need for extra restaurant employees to handle UberEats orders. We created a pricing model that was competitive compared to UberEats and DoorDash, while still offering a significant payout to the restaurants and the middleman. We found someone in the community who already delivered food for his daughter's kitchen and would be willing to receive extra orders.
+
+Our project, however, faced a number of difficulties. While we were interviewing restaurants, ICE raided the CMP community. Many restaurants had migrant workers who could not come into work, and more distrust within the community towards outsiders was created; we thus could not interview restaurateurs at their restaurants, losing context. ENLACE had a significant hand in ensuring our project could still go through with new protocols, and we thank them immensely for their diligent work. Secondly, we could not get our stakeholders to meet at once. Various people either had different time commitments, lost interest in the project, or did not feel comfortable expanding their restaurants just yet. We learned from our collaborators at ENLACE that our case wasn't unique--it was typical for a project to fall through due to parties constantly rescheduling meetings and eventually cancelling, either out of busyness, lack of interest, or distrust. We were still able to present our business plan and a mockup delivery website to ENLACE, who had plans on hiring someone to serve as a liaison for all of the CMP businesses and could thus continue the project.
+
+When I started research for this project in October 2024, I only knew a little bit about Puerto Rican culture. I lacked the confidence to pitch business proposals to receptionists at corporate offices. I was usually juggling multiple different activities and classes and not providing much effort to any of them. Being able to spend time in Puerto Rico, being in situations well outside of my comfort zone, and experiencing focusing my time on just one project lent me a greater appreciation for Puerto Rican culture and increased my confidence and made me much more relaxed. Heck, when I started this project, I did not expect that I'd get over my fear of heights by the end, but thanks to many of the activities I did in Puerto Rico, I did! I've achieved quite a bit through my technical projects, but when I think about which project would help me the most in the workforce, it would have to be this one.`
+	},
+	{
 		title: 'UAV-UGV Collaboration System',
 		date: 'August 2024 - May 2025',
 		blurb:
@@ -76,7 +156,17 @@ We presented our project at Draper's annual Capstone Symposium and won first pla
 				alt: 'UAV perched atop a rock'
 			}
 		],
-		labels: ['C++', 'ESP-32', 'PlatformIO', 'Computer Vision', 'AprilTags', 'WiFi Communication']
+		labels: [
+			'Stakeholder Interaction',
+			'Problem-Solving',
+			'C++',
+			'ESP-32',
+			'PlatformIO',
+			'Computer Vision',
+			'AprilTags',
+			'WiFi Communication',
+			'Electrical Design'
+		]
 	},
 	{
 		title: 'Violet: Handwriting to Text Converter',
@@ -104,7 +194,17 @@ The entire project took me two days to complete, winning me an Honorable Mention
 				alt: 'Some of the code I was writing during the hackathon'
 			}
 		],
-		labels: ['Rust', 'React', 'SvelteJS', 'Python', 'TensorFlow', 'AWS', 'GitHub Actions'],
+		labels: [
+			'Problem-Solving',
+			'Market Gaps',
+			'Rust',
+			'React',
+			'SvelteJS',
+			'Python',
+			'TensorFlow',
+			'AWS',
+			'GitHub Actions'
+		],
 		projectUrl: 'https://devpost.com/software/violet-yrafct'
 	}
 ];
